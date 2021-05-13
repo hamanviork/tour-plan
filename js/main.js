1c58b55881eda72587d44b775dc501cd0eca20b2
hamanivork <vmkolls@gmail.com>
2021-05-13 19:46:25 +0400

    fix: slider, add new photos)

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 const swiper = new Swiper('.swiper-container', {
   loop: true,
+  effect: 'coverflow',
   navigation: {
     nextEl: '.slider-button--next',
     prevEl: '.slider-button--prev',
   },
-
 });
