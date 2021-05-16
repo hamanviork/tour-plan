df0d1e8be8af05c7cbc70c4e558ee1fc46834673
hamanivork <vmkolls@gmail.com>
2021-05-16 15:04:34 +0400

    fix: reviews, but not completely...

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
-const swiper = new Swiper('.swiper-container', {
+var swiper1 = new Swiper('.swiper-container', {
   loop: true,
   effect: 'coverflow',
   navigation: {
     nextEl: '.slider-button--next',
     prevEl: '.slider-button--prev',
+  },
+});
+
+const swiper2 = new Swiper('.reviews-slider', {
+  loop: true,
+  effect: 'coverflow',
+  navigation: {
+    nextEl: '.reviews-slide-button-next',
+    prevEl: '.reviews-slide-button-prev',
   },
 });
