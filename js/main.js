f7a8f0c4b13d80754b21b2680051f51e385a045e
hamanivork <vmkolls@gmail.com>
2021-05-23 19:43:16 +0400

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -14,3 +14,9 @@ const swiper2 = new Swiper('.reviews-slider', {
     nextEl: '.reviews-btns__button_next',
     prevEl: '.reviews-btns__button_prev',
   },});
+
+var menuButton = document.querySelector(".mobile-menu");
+
+menuButton.addEventListener("click", function() {
+  document.querySelector(".navbar").classList.toggle('navbar--visible');
+});
